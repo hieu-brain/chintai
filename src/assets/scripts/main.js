909f1fa2d110381d5b46ae4591b8d4663372e057
Hieu <hieu@brain-net.co.jp>
2018-01-17 00:49:42 +0900

    update header

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -19,28 +19,75 @@
 /* eslint-env browser */
 (function() {
   'use strict';
-  $('.carousel').carousel()
+  $('.carousel').carousel();
   // Your custom JavaScript goes here
-  // $('.related-js').slick({
-  //   responsive: [{
-  //     breakpoint: 99999,
-  //     settings: "unslick"
-  //   },
-  //     {
-  //       breakpoint: 641,
-  //       settings: {
-  //         centerMode: true,
-  //         centerPadding: '12%',
-  //         slidesToShow: 1,
-  //         arrows: false,
-  //         infinite: true,
-  //         autoplay: false,
-  //         autoplaySpeed: 3000,
-  //       }
-  //     }]
-  // });
-  // $('.related-js').slick('resize');
-  // $(window).on('resize orientationchange', function() {
-  //   $('.related-js').slick('resize');
-  // });
+  /*
+  * Slider JS
+  * */
+  var number = 0;
+  $('.image-list').on('click',function (event) {
+       number = parseInt($(event.target).attr("number"));
+      $(".image-justify").eq(0).attr('src','assets/images/'+setImage(number))
+  })
+  $('.prev-button').on('click',function () {
+    number--;
+      number < 0 ? number = 4 : false ;
+    $(".image-justify").eq(0).attr('src','assets/images/'+setImage(number))
+  })
+  $('.next-button').on('click',function () {
+    number++;
+    number > 4 ? number = 0 : false ;
+    $(".image-justify").eq(0).attr('src','assets/images/'+setImage(number))
+  })
+  function setImage(index) {
+    var image;
+    switch(index) {
+      case 0 : image="detail_img_02.jpg"; break;
+      case 1 : image="detail_img_03.jpg"; break;
+      case 2 : image="detail_img_04.jpg"; break;
+      case 3 : image="detail_img_05.jpg"; break;
+      case 4 : image="detail_img_06.jpg"; break;
+    }
+    return image;
+  }
+})();
+(function() {
+  var itemList = '.list-item';
+  var pageList = '.pagination';
+  var pageItem = '<li class="page-item number"><a class="page-link bg-light text-muted" href="javascript:void(0);" data-page="@page">@page</a></li>';
+  var limit = 20;
+  var current = 1;
+  var items = $(itemList).children();
+  var pages = Math.ceil(items.length/limit);
+  var goTo = function (index) {
+    if (index > 0 && index <= pages) {
+      current = index;
+      index --;
+      $(itemList).children().hide();
+      $(pageList).find('[aria-label="Previous"]').attr('data-page', current - 1);
+      $(pageList).find('[aria-label="Next"]').attr('data-page', current + 1);
+      $(pageList).find('[data-page="' + current + '"]').parents('.number.page-item').addClass('active').siblings().removeClass('active');
+      var i = 20 * index;
+      while (i < 20 * (index + 1)) {
+        $(itemList).children().eq(i).show();
+        i++;
+      }
+    }
+  };
+  var init = function () {
+    $(pageList).find('[aria-label="First"]').attr('data-page', current);
+    $(pageList).find('[aria-label="Last"]').attr('data-page', pages);
+    for (var i = 1; i <= pages; i++) {
+      var page = pageItem.replace(/@page/g, i);
+      $(pageList).find('li:nth-last-of-type(2)').before(page);
+    };
+    $(pageList).children().each(function() {
+      var $this = $(this).find('.page-link');
+      $this.click(function() {
+        goTo(parseInt($this.attr('data-page')));
+      });
+    });
+    goTo(current);
+  }
+  init();
 })();
